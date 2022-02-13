420dcfed3706526c5b1fccabe1c3eaf4511a7fae
julienjamme <julien.jamme@protonmail.com>
2022-02-13 17:26:57 +0100

    add spatial layers to preparation data

--- leaflet/00_prepa_data.R
+++ leaflet/00_prepa_data.R
@@ -4,6 +4,8 @@
 # Require R>4.1.1
 # Packages : data.table and dplyr
 
+source("settings_s3.R", encoding = "UTF-8") #S3 connexion settings
+
 list_files <- list(dw = list(name = "base-ic-logement-2018_csv", num = "5650749"),
                    di = list(name = "base-ic-diplomes-formation-2018_csv", num = "5650712")
 )
@@ -19,7 +21,6 @@ lapply(
     unzip(zip_file,exdir = "data/")
   }
 )
-
 
 dipl_tb <- data.table::fread(
   "data/base-ic-diplomes-formation-2018.CSV",
@@ -41,7 +42,34 @@ dw_tb <- data.table::fread(
   dplyr::as_tibble()
 str(dw_tb)
 
-save(dipl_tb, dw_tb, file = "data/iris_marseille_census_2018_dwell_dipl.RData")
+#spatial layers : iris and municipal districts of Marseille
+
+marseille_iris_sf <- 
+  aws.s3::s3read_using(
+    FUN = sf::st_read,
+    object = "fonds/iris_bdtopo_dep_13_2021.gpkg",
+    bucket = "julienjamme",
+    opts = list("region" = "")
+  ) |>
+  dplyr::filter(depcom %in% 13201:13216)
+
+str(marseille_iris_sf)
+
+marseille_armf_sf <- 
+  aws.s3::s3read_using(
+    FUN = sf::st_read,
+    object = "fonds/armf_bdtopo_dep_13_2021.gpkg",
+    bucket = "julienjamme",
+    opts = list("region" = "")
+  ) |>
+  dplyr::filter(code %in% 13201:13216)
+
+str(marseille_armf_sf)
+
+save(
+  dipl_tb, dw_tb, marseille_armf_sf, marseille_iris_sf,
+  file = "data/iris_marseille_census_2018_dwell_dipl_and_sf.RData"
+)
 
 
 
