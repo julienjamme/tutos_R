7e9098514ed6d667b993b0874f31bf0b69c87edd
Julien Jamme <julien.jamme@protonmail.com>
2022-03-04 13:51:05 +0000

    s3 storage and postgis storage of rpg

--- postgis/connect.R
+++ postgis/connect.R
@@ -18,11 +18,10 @@ conn <- connecter(user, password)
 
 DBI::dbListObjects(conn)
 
-parcelles_rpg_R28 <- sf::st_read("../../work/RPG_2-0__SHP_LAMB93_R28_2021-01-01/RPG/1_DONNEES_LIVRAISON_2020/RPG_2-0_SHP_LAMB93_R28_2020/PARCELLES_GRAPHIQUES.shp")
-parcelles_rpg_FR_01 <- sf::st_read("../../work/RPG_2-0__SHP_LAMB93_R28_2021-01-01/RPG/1_DONNEES_LIVRAISON_2020/RPG_2-0_SHP_LAMB93_R28_2020/PARCELLES_GRAPHIQUES.shp")
-parcelles_rpg_FR_02 <- sf::st_read("../../work/RPG_2-0__SHP_LAMB93_R28_2021-01-01/RPG/1_DONNEES_LIVRAISON_2020/RPG_2-0_SHP_LAMB93_R28_2020/PARCELLES_GRAPHIQUES.shp")
+# NORMANDY
 
-# sf::st_write(parcelles_rpg_R28, dsn = "parcelles_rpg_R28_2020.gpkg")
+parcelles_rpg_R28 <- sf::st_read("../../work/RPG_2-0__SHP_LAMB93_R28_2021-01-01/RPG/1_DONNEES_LIVRAISON_2020/RPG_2-0_SHP_LAMB93_R28_2020/PARCELLES_GRAPHIQUES.shp")
+str(parcelles_rpg_R28)
 
 aws.s3::s3write_using(
   parcelles_rpg_R28,
@@ -34,22 +33,74 @@ aws.s3::s3write_using(
   opts = list("region" = "")
 )
 
+conn <- connecter(user, password)
+
+query <- 'CREATE TABLE parcelles_rpg.r28
+  (id_parcel VARCHAR(8),
+  surf_parc REAL,
+  code_cultu CHAR(3),
+  code_group VARCHAR(2),
+  culture_d1 CHAR(3),
+  culture_d2 CHAR(3),
+  geometry GEOMETRY(MULTIPOLYGON, 2154)
+  );
+'
+
+dbSendQuery(conn, query)
+dbListTables(conn)
+
+sf::st_write(
+  obj = parcelles_rpg_R28 |> rename_with(tolower),
+  dsn = conn,
+  Id(schema = 'parcelles_rpg', table = 'r28'),
+  append = TRUE
+)
+
+r28_head <- dbGetQuery(conn, 'SELECT * FROM parcelles_rpg.r28 LIMIT 10;')
+r28_head_sf <- st_read(conn, 'SELECT * FROM parcelles_rpg.r28 LIMIT 10;')
+
+# FRANCE METROPOLITAINE
+
+parcelles_rpg_FR <- sf::st_read("../../RPG_2-0_2-0_GPKG_LAMB93_FR_2021-01-01/RPG/1_DONNEES_LIVRAISON_2020/RPG_2-0_GPKG_LAMB93_FR_2020/PARCELLES_GRAPHIQUES.gpkg")
+str(parcelles_rpg_FR)
+
+# CLOUD STORAGE
+
 aws.s3::s3write_using(
-  parcelles_rpg_FR_01,
+  parcelles_rpg_FR,
   FUN = sf::st_write,
   delete_layer = TRUE,
   col.names = TRUE,
-  object = "ign/parcelles_rpg_FR01_2020.gpkg",
+  object = "ign/parcelles_rpg_FR_2020.gpkg",
   bucket = "julienjamme",
   opts = list("region" = "")
 )
 
-aws.s3::s3write_using(
-  parcelles_rpg_FR_02,
-  FUN = sf::st_write,
-  delete_layer = TRUE,
-  col.names = TRUE,
-  object = "ign/parcelles_rpg_FR02_2020.gpkg",
-  bucket = "julienjamme",
-  opts = list("region" = "")
+# DB STORAGE
+
+query <- 'CREATE TABLE parcelles_rpg.fr
+  (id_parcel VARCHAR(8),
+  surf_parc REAL,
+  code_cultu CHAR(3),
+  code_group VARCHAR(2),
+  culture_d1 CHAR(3),
+  culture_d2 CHAR(3),
+  geometry GEOMETRY(MULTIPOLYGON, 2154)
+  );
+'
+
+dbSendQuery(conn, query)
+dbListTables(conn)
+
+sf::st_write(
+  obj = parcelles_rpg_FR |> rename_with(tolower),
+  dsn = conn,
+  Id(schema = 'parcelles_rpg', table = 'fr'),
+  append = TRUE
 )
+
+fr_head <- dbGetQuery(conn, 'SELECT * FROM parcelles_rpg.fr LIMIT 10;')
+fr_head_sf <- st_read(conn, 'SELECT * FROM parcelles_rpg.fr LIMIT 10;')
+
+
+
